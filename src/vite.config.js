228af715e4e09b5b2ecaaeaa5628afa0dc4f861d
boner.engineer <boner.engineer@outlook.jp>
2023-10-30 14:17:26 +0900

    HotReload add

--- src/vite.config.js
+++ src/vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
         }),
         react(),
     ],
+    server: {
+        // 以下のパラメータを指定しないとapp.css, app.jsのURLが0.0.0.0になってしまうため、明示的にlocalhostに変更
+        hmr: {
+            host: 'localhost'
+        },
+        // Windowsアプリでファイル編集した際に監視されない問題があるため、usePolling:trueにすることで監視を強制させる
+        // https://vitejs.dev/config/server-options.html#server-watch
+        watch: {
+            usePolling: true
+        }
+    }
 });
